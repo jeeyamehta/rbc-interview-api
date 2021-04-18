3f527b69163eb730493cb316b11de26bfe85b9c6
jeeyamehta <jmgrd-adm@yahoo.com>
2021-04-18 15:28:47 -0400

    comments

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import tornado.web
 import json
 import os
 
+# Main handler
 class MainHandler(tornado.web.RequestHandler):
+  def set_default_headers(self):
+      self.set_header("Access-Control-Allow-Origin", "*")
+      self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+      self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
+  # get food items from dictionary.json file
   def get(self):
     with open('dictionary.json') as f:
       food = json.loads(f.read())
       self.write(food)
       f.close()
 
+# make API app with tornado
 def make_app():
   return tornado.web.Application([
       (r"/", MainHandler),
